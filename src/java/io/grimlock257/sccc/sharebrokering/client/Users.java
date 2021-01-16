fbbadeb9ada4af9cd1ed51536019f19c85feac98
Adam Watson <adamwatsonoa@gmail.com>
2021-01-16 14:08:49 +0000

    [SCCC-78] Add ClientResponseModel to encapsulate success status, set user cookiers for 'guid' and 'role'

--- src/java/io/grimlock257/sccc/sharebrokering/client/Users.java
+++ src/java/io/grimlock257/sccc/sharebrokering/client/Users.java
@@ -1,8 +1,11 @@
 package io.grimlock257.sccc.sharebrokering.client;
 
+import io.grimlock257.sccc.sharebrokering.client.model.ClientResponseModel;
 import io.grimlock257.sccc.ws.LoginResponse;
 import io.grimlock257.sccc.ws.ShareBrokering;
 import io.grimlock257.sccc.ws.ShareBrokering_Service;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Users
@@ -49,40 +52,50 @@ public class Users {
      * @param username The users username
      * @param password The users password
      * @param currency The users fund currency
-     * @return A string representing an HTML dialog box with the appropriate message within (success or failure)
+     * @return A ClientResponseModel containing boolean of success, and a message representing an HTML dialog box with the appropriate message within (success or failure)
      */
-    public String register(String firstName, String lastName, String username, String password, String currency) {
+    public ClientResponseModel register(String firstName, String lastName, String username, String password, String currency) {
         if (firstName == null || lastName == null || username == null || password == null || currency == null) {
-            return "<div class='bg-danger p-2 mb-3'>Sorry, something went wrong. It appears some form information is missing - please try again.</div>";
+            return new ClientResponseModel(false, "<div class='bg-danger p-2 mb-3'>Sorry, something went wrong. It appears some form information is missing - please try again.</div>");
         }
 
         Boolean registerSuccess = port.registerUser(firstName, lastName, username, password, currency);
 
         if (registerSuccess) {
-            return "<div class='bg-success p-2 mb-3'>You've successfully registered!</div>";
+            return new ClientResponseModel(true, "<div class='bg-success p-2 mb-3'>You've successfully registered!</div>");
         } else {
-            return "<div class='bg-danger p-2 mb-3'>Your registration has failed. Please try again.</div>";
+            return new ClientResponseModel(false, "<div class='bg-danger p-2 mb-3'>Your registration has failed. Please try again.</div>");
         }
     }
 
     /**
      * Attempt to log the user in with the provided details
      *
+     * @param response The response object from the call site page
      * @param username The supplied username
      * @param password The supplied password
-     * @return A string representing an HTML dialog box with the appropriate message within (success or failure)
+     * @return A ClientResponseModel containing boolean of success, and a message representing an HTML dialog box with the appropriate message within (success or failure)
      */
-    public String login(String username, String password) {
+    public ClientResponseModel login(HttpServletResponse response, String username, String password) {
         if (username == null || password == null) {
-            return "<div class='bg-danger p-2 mb-3'>Sorry, something went wrong. It appears some form information is missing - please try again.</div>";
+            return new ClientResponseModel(false, "<div class='bg-danger p-2 mb-3'>Sorry, something went wrong. It appears some form information is missing - please try again.</div>");
         }
 
         LoginResponse loginResponse = port.loginUser(username, password);
 
         if (loginResponse.isSuccessful()) {
-            return "<div class='bg-success p-2 mb-3'>You've successfully logged in!</div>";
+            Cookie guidCookie = new Cookie("guid", loginResponse.getGuid());
+            guidCookie.setMaxAge(60 * 60 * 24);
+
+            Cookie roleCookie = new Cookie("role", loginResponse.getRole().toString());
+            roleCookie.setMaxAge(60 * 60 * 24);
+
+            response.addCookie(guidCookie);
+            response.addCookie(roleCookie);
+
+            return new ClientResponseModel(true, "<div class='bg-success p-2 mb-3'>You've successfully logged in!</div>");
         } else {
-            return "<div class='bg-danger p-2 mb-3'>Your login has failed. Please try again.</div>";
+            return new ClientResponseModel(false, "<div class='bg-danger p-2 mb-3'>Your login has failed. Please try again.</div>");
         }
     }
 }
